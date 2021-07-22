a6a2ce1c32c1a9ecd6b73d1e32f2e2b82f05b121
sagarrao1 <sagarrao1@gmail.com>
2021-07-22 19:38:03 +0530

    updated Hello-Service Impl

---
+++ Eclipse_Demo/Hello-serviceImpl/src/main/java/com/sagar/service/HelloServiceImpl.java
@@ -0,0 +1,12 @@
+package com.sagar.service;
+
+public class HelloServiceImpl {
+	public String getMessage() {
+		return "Hello world!";
+	}
+
+//	public static void main(String[] args) {
+//		HelloServiceImpl sr=  new HelloServiceImpl();
+//		System.out.println(sr.getMessage());
+//	}
+}
