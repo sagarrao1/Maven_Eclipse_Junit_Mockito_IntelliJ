d81013d6cc9c425a7d86dfdebf46f06df79a4078
sagarrao1 <sagarrao1@gmail.com>
2021-07-23 11:47:21 +0530

    removed toString

--- Eclipse_Demo/Git_Sample_Project/src/main/java/com/sagar/User.java
+++ Eclipse_Demo/Git_Sample_Project/src/main/java/com/sagar/User.java
@@ -17,10 +17,7 @@ public class User {
 	public void setAge(int age) {
 		this.age = age;
 	}
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", age=" + age + "]";
-	}
+
 	public User(String name, int age) {
 		super();
 		this.name = name;
